e94a1269841683d70fd4c4796f51b91e300f9765
pekonsaysua <pekonsaysua@gmail.com>
2021-05-09 15:57:52 +0700

    int project

---
+++ Assets/crashsounder.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class crashsounder : MonoBehaviour {
+
+    AudioSource audio;
+
+    public AudioClip crashSound;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (crashSound == null) Debug.LogError("crashSound is null on " + gameObject.name);
+        //audio.PlayOneShot(crashSound);
+    }
+}
