7950bc85ba8f986a30f6d3b503fe8c7a5aacf1aa
thuanlv <thuanlv@opentechiz.com>
2021-05-19 00:09:46 +0700

    add main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame() {
+        Application.LoadLevel ("track1");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
